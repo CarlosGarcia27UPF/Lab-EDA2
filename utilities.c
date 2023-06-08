a6d4cddd10cb35e133e7d094b2e30a553c614362
CarlosGarcia27UPF <carlos.garcia27@estudiant.upf.edu>
2023-06-08 17:19:07 +0200

    Third Commit

--- utilities.c
+++ utilities.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// Function to insert a new user by taking input from the user
 void insert_new_user() {
     char name[50];
     int age;
@@ -9,6 +10,7 @@ void insert_new_user() {
     char location[50];
     char preferences[5][50];
 
+    // Prompt user for input and read the input values
     printf("Enter name: ");
     getchar(); // To consume the newline character from the input buffer
     fgets(name, sizeof(name), stdin);
@@ -34,6 +36,7 @@ void insert_new_user() {
     const char *prefs[5] = {preferences[0], preferences[1], preferences[2], preferences[3], preferences[4]};
     User new_user = create_user(name, age, email, location, prefs);
 
+    // Add the new user and display the result
     if (add_user(new_user)) {
         printf("New user added successfully!\n");
     } else {
@@ -41,13 +44,16 @@ void insert_new_user() {
     }
 }
 
+// Function to display the list of users
 void list_users() {
     display_users();
 }
 
+// Function to display a menu for a specific user and perform the selected actions
 void user_menu(User *user) {
     int choice;
 
+    // Display user menu and prompt for input until the user chooses to exit
     do {
         printf("\n--- %s's Menu ---\n", user->name);
         printf("1. Send friend request\n");
@@ -58,6 +64,7 @@ void user_menu(User *user) {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
+        // Perform the selected action based on the user's input
         switch (choice) {
             case 1: {
                 char name[50];
@@ -67,6 +74,7 @@ void user_menu(User *user) {
                 name[strcspn(name, "\n")] = 0; // Remove newline character from the input
                 User *to_user = get_user_by_name(name);
 
+                // Send a friend request and display the result
                 if (send_friend_request(user, to_user)) {
                     printf("Friend request sent successfully!\n");
                 } else {
@@ -82,6 +90,7 @@ void user_menu(User *user) {
                 name[strcspn(name, "\n")] = 0; // Remove newline character from the input
                 User *from_user = get_user_by_name(name);
 
+                // Accept a friend request and display the result
                 if (accept_friend_request(from_user, user)) {
                     printf("Friend request accepted!\n");
                 } else {
@@ -96,6 +105,7 @@ void user_menu(User *user) {
                 fgets(content, sizeof(content), stdin);
                 content[strcspn(content, "\n")] = 0; // Remove newline character from the input
 
+                // Add a post and display the result
                 if (add_post(user, content)) {
                     printf("Post added successfully!\n");
                 } else {
@@ -104,14 +114,16 @@ void user_menu(User *user) {
                 break;
             }
             case 4:
+                // Display the user's posts
                 display_posts(user);
                 break;
             case 5:
+                // Exit the user menu
                 printf("Returning to main menu...\n");
                 break;
             default:
+                // Handle invalid input
                 printf("Invalid choice. Please try again.\n");
         }
     } while (choice != 5);
 }
-
